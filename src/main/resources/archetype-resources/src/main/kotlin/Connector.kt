51a2e7250ed31ed8f7b678fa4af040f54afdd4d0
Adrien <adrien.lachambre@bonitasoft.com>
2020-03-17 11:15:33 +0100

    format

--- src/main/resources/archetype-resources/src/main/kotlin/Connector.kt
+++ src/main/resources/archetype-resources/src/main/kotlin/Connector.kt
@@ -1,31 +1,33 @@
-package ${groupId}
+package $
+
+{ groupId }
 
 import org.bonitasoft.engine.connector.AbstractConnector
 import org.bonitasoft.engine.connector.ConnectorValidationException
 import org.slf4j.LoggerFactory;
 
 class Connector : AbstractConnector {
-	
-	constructor() : super()
-	
-	val logger = LoggerFactory.getLogger(Connector::class.java.name)
-	val defaultInput = "defaultInput"
-	
+
+    constructor() : super()
+
+    val logger = LoggerFactory.getLogger(Connector::class.java.name)
+    val defaultInput = "defaultInput"
+
     override fun validateInputParameters() {
-	    checkMandatoryStringInput(defaultInput)
-	}
-	
-	fun checkMandatoryStringInput(inputName : String) {
-		val value = getInputParameter(inputName)
-		if (!(value is String)) {
-			throw ConnectorValidationException(this, "'$inputName' parameter must be a String.")
-		} else if(value.isNullOrEmpty()) {
-			throw ConnectorValidationException(this, "Mandatory parameter '$inputName' is missing.")
-		}
-	}
-	
-	override fun executeBusinessLogic() {
-		val value = getInputParameter(defaultInput)
-		logger.info("Default input: $value")
-	}
+        checkMandatoryStringInput(defaultInput)
+    }
+
+    fun checkMandatoryStringInput(inputName: String) {
+        val value = getInputParameter(inputName)
+        if (!(value is String)) {
+            throw ConnectorValidationException(this, "'$inputName' parameter must be a String.")
+        } else if (value.isNullOrEmpty()) {
+            throw ConnectorValidationException(this, "Mandatory parameter '$inputName' is missing.")
+        }
+    }
+
+    override fun executeBusinessLogic() {
+        val value = getInputParameter(defaultInput)
+        logger.info("Default input: $value")
+    }
 }
--- src/main/resources/archetype-resources/src/test/kotlin/ConnectorTest.kt
+++ src/main/resources/archetype-resources/src/test/kotlin/ConnectorTest.kt
@@ -5,30 +5,30 @@ import org.bonitasoft.engine.connector.ConnectorValidationException
 
 class ConnectorTest {
 
-	@Test(expected = ConnectorValidationException::class)
-	fun should_throw_exception_if_mandatory_input_is_missing() {
-		val connector = Connector()
+    @Test(expected = ConnectorValidationException::class)
+    fun should_throw_exception_if_mandatory_input_is_missing() {
+        val connector = Connector()
         connector.validateInputParameters()
-	}
-	
-	@Test(expected = ConnectorValidationException::class)
-	fun should_throw_exception_if_mandatory_input_is_empty() {
-		val connector = Connector()
-		connector.setInputParameters(mapOf("defaultInput" to ""))
+    }
+    
+    @Test(expected = ConnectorValidationException::class)
+    fun should_throw_exception_if_mandatory_input_is_empty() {
+        val connector = Connector()
+        connector.setInputParameters(mapOf("defaultInput" to ""))
         connector.validateInputParameters()
-	}
-	
-	@Test(expected = ConnectorValidationException::class)
-	fun should_throw_exception_if_mandatory_input_is_not_a_string() {
-		val connector = Connector()
-		connector.setInputParameters(mapOf("defaultInput" to 38))
+    }
+    
+    @Test(expected = ConnectorValidationException::class)
+    fun should_throw_exception_if_mandatory_input_is_not_a_string() {
+        val connector = Connector()
+        connector.setInputParameters(mapOf("defaultInput" to 38))
         connector.validateInputParameters()
-	}
-	
-	@Test
-	fun should_accept_valid_input() {
-		val connector = Connector()
-		connector.setInputParameters(mapOf("defaultInput" to "valid"))
+    }
+    
+    @Test
+    fun should_accept_valid_input() {
+        val connector = Connector()
+        connector.setInputParameters(mapOf("defaultInput" to "valid"))
         connector.validateInputParameters()
-	}
+    }
 }
