7d9be0d0d46e6ab72ab1f9b588a7d050127c56a3
Adrien <adrien.lachambre@bonitasoft.com>
2020-03-19 11:43:27 +0100

    feat(doc) add comments in generated files
    [STUDIO-3483](https://bonitasoft.atlassian.net/browse/STUDIO-3483)

--- src/test/resources/projects/testKotlinProject/reference/src/main/kotlin/com/company/bonitasoft/Connector.kt
+++ src/test/resources/projects/testKotlinProject/reference/src/main/kotlin/com/company/bonitasoft/Connector.kt
@@ -10,6 +10,12 @@ class Connector : AbstractConnector {
     val defaultInput = "defaultInput"
     val defaultOutput = "defaultOutput"
 
+    /**
+     * Perform validation on the inputs defined on the connector definition (src/main/resources/connectorKotlin.def)
+     * You should: 
+     * - validate that mandatory inputs are presents
+     * - validate that the content of the inputs is coherent with your use case (e.g: validate that a date is / isn't in the past ...)
+     */
     override fun validateInputParameters() {
         checkMandatoryStringInput(defaultInput)
     }
@@ -23,9 +29,24 @@ class Connector : AbstractConnector {
         }
     }
 
+    /**
+     * Core method: 
+     * - Execute all the business logic of your connector using the inputs (connect to an external service, compute some values ...).
+     * - Set the output of the connector execution. If outputs are not set, connector fails.
+     */
     override fun executeBusinessLogic() {
         val value = getInputParameter(defaultInput)
         println("Default input: $value")
         setOutputParameter(defaultOutput, "$value - output")
     }
+    
+    /**
+     * [Optional] Open a connection to remote server
+     */
+    override fun connect(){}
+    
+    /**
+     * [Optional] Close connection to remote server
+     */
+    override fun disconnect(){}
 }
