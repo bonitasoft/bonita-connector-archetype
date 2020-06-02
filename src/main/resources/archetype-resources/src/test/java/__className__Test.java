21be293e34fd8c19326dccf487d63510c188c230
hugues.bretin <hugues.bretin@bonitasoft.com>
2020-06-02 18:20:09 +0200

    feat(mvn): add junit5 support for java

--- src/main/resources/archetype-resources/src/test/java/__className__Test.java
+++ src/main/resources/archetype-resources/src/test/java/__className__Test.java
@@ -1,50 +1,59 @@
 package ${package};
 
-import static org.junit.Assert.assertTrue;
+import org.bonitasoft.engine.connector.ConnectorException;
+import org.bonitasoft.engine.connector.ConnectorValidationException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
-import org.bonitasoft.engine.connector.ConnectorException;
-import org.bonitasoft.engine.connector.ConnectorValidationException;
-import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ${className}Test {
 
-    @Test(expected = ConnectorValidationException.class)
-    public void should_throw_exception_if_mandatory_input_is_missing() throws ConnectorValidationException {
-        ${className} connector = new ${className}();
-        connector.validateInputParameters();
-    }
+    ${className} connector;
 
-    @Test(expected = ConnectorValidationException.class)
-    public void should_throw_exception_if_mandatory_input_is_empty() throws ConnectorValidationException {
-        ${className} connector = new ${className}();
-        Map<String, Object> parameters = new HashMap<>();
-        parameters.put(${className}.DEFAULT_INPUT, "");
-        connector.setInputParameters(parameters);
-        connector.validateInputParameters();
-    }
-
-    @Test(expected = ConnectorValidationException.class)
-    public void should_throw_exception_if_mandatory_input_is_not_a_string() throws ConnectorValidationException {
-        ${className} connector = new ${className}();
-        Map<String, Object> parameters = new HashMap<>();
-        parameters.put(${className}.DEFAULT_INPUT, 38);
-        connector.setInputParameters(parameters);
-        connector.validateInputParameters();
+    @BeforeEach
+    void setUp() {
+        connector = new ${className}();
     }
 
     @Test
-    public void should_create_output_for_valid_input() throws ConnectorException {
-        ${className} connector = new ${className}();
+    void should_throw_exception_if_mandatory_input_is_missing() {
+        assertThrows(ConnectorValidationException.class, () ->
+                connector.validateInputParameters()
+        );
+    }
+
+    @Test
+    void should_throw_exception_if_mandatory_input_is_empty() {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put(${className}.DEFAULT_INPUT, "");
+        connector.setInputParameters(parameters);
+        assertThrows(ConnectorValidationException.class, () ->
+                connector.validateInputParameters()
+        );
+    }
+
+    @Test
+    void should_throw_exception_if_mandatory_input_is_not_a_string() {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put(${className}.DEFAULT_INPUT, 38);
+        connector.setInputParameters(parameters);
+        assertThrows(ConnectorValidationException.class, () ->
+                connector.validateInputParameters()
+        );
+    }
+
+    @Test
+    void should_create_output_for_valid_input() throws ConnectorException {
         Map<String, Object> parameters = new HashMap<>();
         parameters.put(${className}.DEFAULT_INPUT, "valid");
         connector.setInputParameters(parameters);
         Map<String, Object> outputs = connector.execute();
-        assertTrue("Expected 'valid - output' as output.",
-                Objects.equals(outputs.get("defaultOutput"), "valid - output"));
+        assertThat(outputs.get("defaultOutput")).isEqualTo("valid - output");
     }
 
 }
