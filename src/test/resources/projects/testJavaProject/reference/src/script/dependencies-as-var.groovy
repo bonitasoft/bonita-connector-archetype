56b8cb5bef2186f99ee0bb66a2133b607585d2e4
Romain Bioteau <romain.bioteau@bonitasoft.com>
2022-06-20 15:39:42 +0200

    fix(impl): Handle deps with classifier (#47)
    Closes STUDIO-4371

--- src/test/resources/projects/testJavaProject/reference/src/script/dependencies-as-var.groovy
+++ src/test/resources/projects/testJavaProject/reference/src/script/dependencies-as-var.groovy
@@ -8,7 +8,8 @@ builder.jarDependencies {
             .findAll { artifact -> artifact.scope == "compile" || artifact.scope == "runtime" }
             .sort { artifact -> artifact.artifactId }
             .each { artifact ->
-                jarDependency("${artifact.artifactId}-${artifact.version}.${artifact.type}")
+                jarDependency(artifact.classifier ? "${artifact.artifactId}-${artifact.version}-${artifact.classifier}.${artifact.type}" 
+                                                   : "${artifact.artifactId}-${artifact.version}.${artifact.type}")
             }
 }
 def deps = xml.toString()
